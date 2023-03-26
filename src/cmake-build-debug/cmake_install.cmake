6e204acf134c2ef975ac80d63bc198d504304fda
Jacopo <jaga.jacopo@gmail.com>
2023-03-26 22:47:26 +0200

    Static linking of external libraries
    The .exe file is now portable

--- src/cmake-build-debug/cmake_install.cmake
+++ src/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/jagaj/Documents/jacopo/Uni/Secondo Anno/Algoritmi e Strutture Dati/Laboratorio/LabAlgoritmiEStruttureDati/Progetto
+# Install script for directory: C:/Users/jagaj/Documents/jacopo/Uni/Secondo Anno/Algoritmi e Strutture Dati/Laboratorio/ProgettoASD_22_23/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/jagaj/Documents/jacopo/Uni/Secondo Anno/Algoritmi e Strutture Dati/Laboratorio/LabAlgoritmiEStruttureDati/Progetto/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jagaj/Documents/jacopo/Uni/Secondo Anno/Algoritmi e Strutture Dati/Laboratorio/ProgettoASD_22_23/src/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
